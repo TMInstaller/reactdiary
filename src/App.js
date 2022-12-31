e58280e8291f138d62bf7f85a598dd7a9b792fbf
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-01 04:31:51 +0900

    DiaryItem 생성 및 출력창 커스터마이징

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id:1,
+    author:"백동열",
+    content:"안녕하세요 1",
+    emotion:"5",
+    created_date: new Date().getTime(),
+  },
+  {
+    id:2,
+    author:"백동열2",
+    content:"안녕하세요 2",
+    emotion:"4",
+    created_date: new Date().getTime(),
+  },
+  {
+    id:3,
+    author:"백동열3",
+    content:"안녕하세요 3",
+    emotion:"3",
+    created_date: new Date().getTime(),
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <DiaryEditor />
+      <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
