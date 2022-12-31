bdf6fa32755df278ba9ec3e226b1e3f767a9379d
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-01 02:53:20 +0900

    commit

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,43 +1,35 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
 
-    const [state, setState] = useState({
-        author: "",
-        content: "",
-    });
+  const handleChangeState = (e)=>{
+    console.log(e.target.name);
+    console.log(e.target.value);
+  }
 
-    const [author, setAuthor] = useState("");
-    const [content, setContent] = useState("");
-
-    return (
-        <div className="DiaryEditor">
-            <h2>오늘의 일기</h2>
-            <div>
-                <input
-                    name="author"
-                    value={state.author} 
-                    onChange={(e)=>{
-                        setState({
-                            author:e.target.value,
-                            content: state.content,
-                        });
-                    }}
-                />
-            </div>
-            <div>
-                <textarea
-                    value={state.content} 
-                    onChange={(e)=>{
-                        setState({
-                            author:e.target.value,
-                            content: state.author,
-                        });
-                    }} 
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryEditor;
