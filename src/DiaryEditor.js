f3c34bcf74057539bce7f5cae78d8e4d78bcbfd0
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-01 02:37:12 +0900

    변경내용 커밋

---
+++ src/DiaryEditor.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+
+    const [state, setState] = useState({
+        author: "",
+        content: "",
+    });
+
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("");
+
+    return (
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input
+                    name="author"
+                    value={author} 
+                    onChange={(e)=>{
+                        setAuthor(e.target.value);
+                    }}
+                />
+            </div>
+            <div>
+                <textarea
+                    value={content} 
+                    onChange={(e)=>{
+                        setContent(e.target.value);
+                    }} 
+                />
+            </div>
+        </div>
+    );
+};
+
+export default DiaryEditor;
