c502da16719fbf598ebe1ed0bc8973416531b1e2
TMInstaller <ajvldmlqjqclr@naver.com>
2023-01-01 14:25:39 +0900

    일기 리스트 디자인 추가하기

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,6 +1,5 @@
 import DiaryItem from "./DiaryItem";
 const DiaryList = ({diaryList}) => {
-    console.log(diaryList);
     return(
         <div className="DiaryList">
             <h2>일기 리스트</h2>
